12cad7c10d6ff3ef02413d6422f6157b8c7c88de
Samuel Lee <lsme8821@gmail.com>
2021-05-14 21:43:54 +0900

    Change file name types.hpp to core.hpp

---
+++ liteCV/core/core.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef LCV_CORE_CORE_HPP
+#define LCV_CORE_CORE_HPP
+#include "lcvdef.hpp"
+
+#include "element.hpp"
+#include "point.hpp"
+#include "size.hpp"
+#include "rect.hpp"
+#include "matrix.hpp"
+#endif // LCV_CORE_CORE_HPP
--- liteCV/core/types.hpp
+++
@@ -1,8 +0,0 @@
-#pragma
-#ifndef LCV_CORE_TYPES_HPP
-#define LCV_CORE_TYPES_HPP
-#include "element.hpp"
-#include "point.hpp"
-#include "size.hpp"
-#include "rect.hpp"
-#endif // LCV_CORE_TYPES_HPP
